f106d0fa44111bf13c146f782062161d27f6debc
gui2xhg-hub <gui.2.x.hg@gmail.com>
2026-09-04 18:50:36 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').then(
+          (registration) => {
+            console.log('PWA ServiceWorker registrado com sucesso!', registration.scope);
+          },
+          (err) => {
+            console.log('Falha ao registrar PWA ServiceWorker: ', err);
+          }
+        );
+      });
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
